1aeed60a85f805b55d4d7438dc78a76e855e1c85
ShawnS2020 <shawnathon4@gmail.com>
2023-10-11 19:31:54 -0400

    Cleaned up display.js a little

--- server/public/js/display.js
+++ server/public/js/display.js
@@ -5,17 +5,23 @@ const gameBoards = [];
 
 container.style.height = window.innerHeight + "px";
 
+// Create 6 player spaces
+// A player space contains a game board and a player name
 for (let i = 0; i < 6; i++) {
     const playerSpace = document.createElement('div');
-    playerSpace.classList.add('player-space');
+    const gameBoard = document.createElement('div');
     const playerName = document.createElement('h1');
+
+    playerSpace.classList.add('player-space');
+    gameBoard.classList.add('game-board');
     playerName.classList.add('player-name');
     playerName.innerText = 'Player ' + (i + 1);
-    const gameBoard = document.createElement('div');
-    gameBoard.classList.add('game-board');
+
+    // Create 8 rows of 8 cells each
     for (let j = 0; j < 8; j++) {
         const gameRow = document.createElement('div');
         gameRow.classList.add('game-board-row');
+
         for (let k = 0; k < 8; k++) {
             const gameCell = document.createElement('div');
             gameCell.classList.add('game-board-cell');
@@ -25,6 +31,7 @@ for (let i = 0; i < 6; i++) {
     }
     playerSpace.append(gameBoard, playerName);
     gameBoards.push(gameBoard);
+
     if (i < 3) {
         row1.appendChild(playerSpace);
     } else {
